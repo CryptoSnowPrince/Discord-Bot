8257025d9d868ac2ca3914b60b99df04bb8d7ade
CryptoSnowMickey <topdirector2017@gmail.com>
2022-04-07 14:28:32 +0000

    update

--- bot.js
+++ bot.js
@@ -2,53 +2,18 @@
 // Run dotenv
 require('dotenv').config();
 
-const {Intents, Client, Discord} = require('discord.js');
-// const client = new Discord.Client();
+const {Intents, Client} = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// client.on('interactionCreate', async interaction => {
-//     console.log("passpass");
-//     if (!interaction.isCommand()) return;
-
-//     const { commandName } = interaction;
-
-//     if (commandName === 'ping') {
-//         await interaction.reply('Pong!');
-//         console.log("pass");
-//     } else if (commandName === 'server') {
-//         // await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-//     } else if (commandName === 'user') {
-//         await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-//     }
-// });
-
-// client.on('message', function (user, userID, channelID, message, evt) {
-//     // Our bot needs to know if it will execute a command
-//     // It will listen for messages that will start with `!`
-//     if (message.substring(0, 1) == '!') {
-//         var args = message.substring(1).split(' ');
-//         var cmd = args[0];
-//         args = args.splice(1);
-//         switch (cmd) {
-//             // !ping
-//             case 'ping':
-//                 bot.sendMessage({
-//                     to: channelID,
-//                     message: 'Pong!'
-//                 });
-//                 break;
-//             // Just add any case commands if you want to..
-//         }
-//     }
-// });
-
 client.on('message', msg => {
+    console.log("msg", msg);
     if (msg.content === 'ping') {
-        msg.reply('pong');
+        console.log(msg.user)
+        msg.reply('<@931580130114011136> <@&959549727756677133> <@&960878105046691852> <@&961335781912354819> <@&961520126354653205>  pong');
     }
 });
 
