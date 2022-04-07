30dd5169252b998800f995f159e04815f5c3e607
CryptoSnowMickey <topdirector2017@gmail.com>
2022-04-07 05:43:00 +0000

    update

--- bot.js
+++ bot.js
@@ -1,48 +1,55 @@
-var Discord = require('discord.io');
 
-var logger = require('winston');
+// Run dotenv
+require('dotenv').config();
 
-var auth = require('./auth.json');
+const {Intents, Client, Discord} = require('discord.js');
+// const client = new Discord.Client();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-// Configure logger settings
-
-logger.remove(logger.transports.Console);
-
-logger.add(new logger.transports.Console, {
-    colorize: true
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-logger.level = 'debug';
+// client.on('interactionCreate', async interaction => {
+//     console.log("passpass");
+//     if (!interaction.isCommand()) return;
 
-// Initialize Discord Bot
+//     const { commandName } = interaction;
 
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-});
+//     if (commandName === 'ping') {
+//         await interaction.reply('Pong!');
+//         console.log("pass");
+//     } else if (commandName === 'server') {
+//         // await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+//     } else if (commandName === 'user') {
+//         await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+//     }
+// });
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+// client.on('message', function (user, userID, channelID, message, evt) {
+//     // Our bot needs to know if it will execute a command
+//     // It will listen for messages that will start with `!`
+//     if (message.substring(0, 1) == '!') {
+//         var args = message.substring(1).split(' ');
+//         var cmd = args[0];
+//         args = args.splice(1);
+//         switch (cmd) {
+//             // !ping
+//             case 'ping':
+//                 bot.sendMessage({
+//                     to: channelID,
+//                     message: 'Pong!'
+//                 });
+//                 break;
+//             // Just add any case commands if you want to..
+//         }
+//     }
+// });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-        args = args.splice(1);
-        switch (cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-                break;
-            // Just add any case commands if you want to..
-        }
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
     }
 });
+
+client.login(process.env.DISCORD_TOKEN);
