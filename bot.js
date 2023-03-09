eeedbd378cf80afee57f1ac8a83275135666e467
CryptoSnowPrince <topdirector2017@gmail.com>
2023-03-09 18:42:25 +0100

    update

--- bot.js
+++ bot.js
@@ -1,18 +1,24 @@
-
 // Run dotenv
 require('dotenv').config();
 
-const {Intents, Client} = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const Discord = require('discord.js');
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+const client = new Discord.Client({
+    intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]
 });
 
-client.on('message', msg => {
-    console.log("msg", msg);
-    if (msg.content === 'ping') {
-        msg.reply('<@931580130114011136> <@&959549727756677133> <@&960878105046691852> <@&961335781912354819> <@&961520126354653205>  pong');
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    // console.log(message)
+    if (message.content === 'ping') {
+        message.reply('<@931580130114011136> <@&959549727756677133> <@&960878105046691852> <@&961335781912354819> <@&961520126354653205>  pong');
+    } else if (message.content === '/start') {
+        message.reply('start message received');
+    } else if (message.content === '/help') {
+        message.reply('help message received');
     }
 });
 
